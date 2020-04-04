975c9c940250eb380b995162f637e1d357d31e97
Dominik Mozny <dominik.mozny@sbb.ch>
2020-04-05 00:45:53 +0200

    Add REST GET for / path

--- src/main/java/cz/mozny/dominik/Controller.java
+++ src/main/java/cz/mozny/dominik/Controller.java
@@ -1,10 +1,16 @@
 package cz.mozny.dominik;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @GetMapping("/")
+    public String index() {
+        return "Hello there! I'm running.";
+    }
+
     @RequestMapping(value = "/hello")
     public String getQuestions() {
         return "Hello World";
