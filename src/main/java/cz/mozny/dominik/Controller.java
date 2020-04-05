abe4b53bb9e8844b3a258a1a04688181daf277e4
Dominik Mozny <dominik.mozny@sbb.ch>
2020-04-05 02:07:52 +0200

    Rest endpoint returns JSON

--- src/main/java/cz/mozny/dominik/Controller.java
+++ src/main/java/cz/mozny/dominik/Controller.java
@@ -1,18 +1,12 @@
 package cz.mozny.dominik;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @GetMapping("/")
-    public String index() {
-        return "Hello there! I'm running.";
-    }
-
-    @RequestMapping(value = "/hello")
-    public String getQuestions() {
-        return "Hello World";
+    @RequestMapping(value = "/name", produces = "application/json")
+    public String getName() {
+        return "{\"name\": \"Dominik Mozny\"}";
     }
 }
