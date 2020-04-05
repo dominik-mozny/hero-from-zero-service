6bc1a174106b0eb4bb4847ea8fe11766add60b06
Dominik Mozny <dominik.mozny@sbb.ch>
2020-04-05 02:26:15 +0200

    Allow CrossOrigin

--- src/main/java/cz/mozny/dominik/Controller.java
+++ src/main/java/cz/mozny/dominik/Controller.java
@@ -1,9 +1,11 @@
 package cz.mozny.dominik;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class Controller {
     @RequestMapping(value = "/name", produces = "application/json")
     public String getName() {
